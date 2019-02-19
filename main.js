8e9265d528d4ac8f0e0c47da4071092e6e7d467f
Isaacb22 <ibonalde001@gmail.com>
2019-02-19 14:13:16 -0400

    se crearon las barras de la tabla

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//MODELO
 (function(){
 	self.Board = function(width,height){
 		this.width = width;
@@ -11,13 +12,38 @@
 	self.Board.prototype = {
 		get elements(){
 			var elements = this.bars;
-			elements.push(ball);
+			elements.push(this.ball);
 			return elements;
 		}
 	}
 
 })();
 
+
+// BARRAS
+(function(){
+	self.Bar = function(x,y,width,height,board){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.board = board;
+		this.board.bars.push(this);
+		this.kind = "rectangle";
+	} 
+
+	self.Bar.prototype = {
+		down : function(){
+
+		},
+		up : function(){
+
+		}
+	}
+
+})();
+
+//VISTA	
 (function(){
 	self.BoardView = function(canvas, board){
 		this.canvas = canvas;
@@ -25,14 +51,40 @@
 		this.canvas.height = board.height;
 		this.board = board;
 		this.ctx = canvas.getContext("2d");
+	}
+
+	self.BoardView.prototype = {
+		draw: function(){
+			for (var i = this.board.elements.length - 1; i >= 0; i--) {
+				var el = this.board.elements[i];
+
+				draw(this.ctx,el);
+			}
+		}
+	}
+
+	function draw(ctx,element){
+		if(element !== null && element.hasOwnProperty("kind")){
+			switch(element.kind){
+				case "rectangle":
+					ctx.fillRect(element.x,element.y,element.width,element.height);
+					break;
+		}
+		}
+		
 
 	}
 })();
 
 window.addEventListener("load", main);
 
+//CONTROLADOR	
 function main(){
-	var board = new Board(1000,400);
+	var board = new Board(1000,405);
+	var bar = new Bar(20,100,40,100,board);
+	var bar = new Bar(940,110,40,100,board);   
 	var canvas = document.getElementById('canvas');
 	var board_view = new BoardView(canvas,board);
+
+	board_view.draw();
 }
