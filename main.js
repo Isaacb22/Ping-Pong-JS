35620cbbb170c2768391fd949a41827a94af06af
Isaacb22 <ibonalde001@gmail.com>
2019-02-20 13:32:07 -0400

    Se agregaron el movimiento de las barras y la pelota

--- main.js
+++ main.js
@@ -19,6 +19,21 @@
 
 })();
 
+(function(){
+	self.Ball = function(x,y,radius,board){
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.speed_y = 0;
+		this.speed_x = 3;
+		this.board = board;
+		this.kind = "circle";
+		
+		board.ball = this;  
+
+	}
+})();
+
 
 // BARRAS
 (function(){
@@ -30,14 +45,18 @@
 		this.board = board;
 		this.board.bars.push(this);
 		this.kind = "rectangle";
+		this.speed = 10;
 	} 
 
 	self.Bar.prototype = {
 		down : function(){
-
+			this.y += this.speed;
 		},
 		up : function(){
-
+			this.x -= this.speed;
+		},
+		toString: function(){
+			return "x: "+ this.x + " y: "+ this.y;   
 		}
 	}
 
@@ -54,37 +73,72 @@
 	}
 
 	self.BoardView.prototype = {
+		clean: function(){
+			this.ctx.clearRect(0,0,this.board.width,this.board.height);
+		},
 		draw: function(){
 			for (var i = this.board.elements.length - 1; i >= 0; i--) {
 				var el = this.board.elements[i];
 
 				draw(this.ctx,el);
-			}
+			};
+		},
+		play:function(){
+			this.clean();
+			this.draw();
 		}
 	}
 
 	function draw(ctx,element){
-		if(element !== null && element.hasOwnProperty("kind")){
-			switch(element.kind){
-				case "rectangle":
-					ctx.fillRect(element.x,element.y,element.width,element.height);
-					break;
-		}
-		}
+		switch(element.kind){
+			case "rectangle":
+				ctx.fillRect(element.x,element.y,element.width,element.height);
+				break;
+
+			case "circle":
+				ctx.beginPath();
+				ctx.arc(element.x,element.y,element.radius,0,7);
+				ctx.fill();
+				ctx.closePath();
+				break;
+	}
+		
 		
 
 	}
 })();
 
-window.addEventListener("load", main);
+var board = new Board(1000,405);
+var bar = new Bar(20,100,40,100,board);
+var bar_2 = new Bar(940,120,40,100,board);   
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas,board);
+var ball = new Ball(420,100,10,board);
+
+
+document.addEventListener("keydown",function(ev){
+	ev.preventDefault();
+	if(ev.keyCode == 38){
+		bar.up();
+	}
+	else if(ev.keyCode == 40){
+		bar.down();
+	}else if(ev.keyCode ===   87){
+		//W
+		bar_2.up();
+	}else if(ev.keyCode === 83){
+		//S
+		bar_2.down();
+	}
+
+	console.log(""+bar_2);
+
+});
+
+window.requestAnimationFrame(controller);
 
 //CONTROLADOR	
-function main(){
-	var board = new Board(1000,405);
-	var bar = new Bar(20,100,40,100,board);
-	var bar = new Bar(940,110,40,100,board);   
-	var canvas = document.getElementById('canvas');
-	var board_view = new BoardView(canvas,board);
-
-	board_view.draw();
+function controller(){
+	board_view.play();
+	window.requestAnimationFrame(controller);
 }
